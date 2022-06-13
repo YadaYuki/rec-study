06407c53f03e33eb8ca2699df9a9652d066bb6fa
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-13 12:55:01 +0900

    Add Comedy filt

--- src/popularity.py
+++ src/popularity.py
@@ -1,7 +1,9 @@
-from util.models import RecommendResult, Dataset
-from src.base_recommender import BaseRecommender
 from collections import defaultdict
+
 import numpy as np
+from util.models import Dataset, RecommendResult
+
+from src.base_recommender import BaseRecommender
 
 np.random.seed(0)
 
@@ -11,26 +13,47 @@ class PopularityRecommender(BaseRecommender):
         # 評価数の閾値
         minimum_num_rating = kwargs.get("minimum_num_rating", 200)
 
+        # ジャンルを指定
+        genre = kwargs.get("genre", None)
+
         # 各アイテムごとの平均の評価値を計算し、その平均評価値を予測値として利用する
-        movie_rating_average = dataset.train.groupby("movie_id").agg({"rating": np.mean})
+        movie_rating_average = dataset.train.groupby("movie_id").agg(
+            {"rating": np.mean}
+        )
         # テストデータに予測値を格納する。テストデータのみに存在するアイテムの予測評価値は０とする
         movie_rating_predict = dataset.test.merge(
             movie_rating_average, on="movie_id", how="left", suffixes=("_test", "_pred")
         ).fillna(0)
 
+        movie_id_to_genre_dict = (
+            dataset.item_content[["movie_id", "genre"]]
+            .set_index("movie_id")["genre"]
+            .to_dict()
+        )
+
         # 各ユーザに対するおすすめ映画は、そのユーザがまだ評価していない映画の中から評価値が高いもの10作品とする
         # ただし、評価件数が少ないとノイズが大きいため、minimum_num_rating件以上評価がある映画に絞る
         pred_user2items = defaultdict(list)
-        user_watched_movies = dataset.train.groupby("user_id").agg({"movie_id": list})["movie_id"].to_dict()
-        movie_stats = dataset.train.groupby("movie_id").agg({"rating": [np.size, np.mean]})
+        user_watched_movies = (
+            dataset.train.groupby("user_id")
+            .agg({"movie_id": list})["movie_id"]
+            .to_dict()
+        )
+        movie_stats = dataset.train.groupby("movie_id").agg(
+            {"rating": [np.size, np.mean]}
+        )
         atleast_flg = movie_stats["rating"]["size"] >= minimum_num_rating
         movies_sorted_by_rating = (
-            movie_stats[atleast_flg].sort_values(by=("rating", "mean"), ascending=False).index.tolist()
+            movie_stats[atleast_flg]
+            .sort_values(by=("rating", "mean"), ascending=False)
+            .index.tolist()
         )
 
         for user_id in dataset.train.user_id.unique():
             for movie_id in movies_sorted_by_rating:
-                if movie_id not in user_watched_movies[user_id]:
+                if (movie_id not in user_watched_movies[user_id]) and (
+                    genre in movie_id_to_genre_dict[movie_id]
+                ):
                     pred_user2items[user_id].append(movie_id)
                 if len(pred_user2items[user_id]) == 10:
                     break
